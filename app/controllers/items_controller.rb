318d175c58c31b0a5e8dbb7a1f9c15efedc43a09
shotaogw <shotaogw@gmail.com>
2020-12-29 18:20:06 +0900

    商品出品ページ作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,26 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      return redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :title, :info, :category_id, :status_id, :shipping_fee_id, :presence_id, :delivery_day_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
-class Item < ApplicationRecord
+class Item < ApplicationRecord 
+  validates :image, presence: true
+  validates :title, presence: true
+  validates :info,  presence: true
+  validates :category_id,     numericality: { other_than: 1, message: 'select' }
+  validates :status_id,       numericality: { other_than: 1, message: 'select' }
+  validates :shipping_fee_id, numericality: { other_than: 1, message: 'select' }
+  validates :prefecture_id,   numericality: { other_than: 1, message: 'select' }
+  validates :delivery_day_id, numericality: { other_than: 1, message: 'select' }
+  validates :price, presence: true,
+    format: { with: /\A[0-9]+\z/, message: 'half-width number' },
+    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
   belongs_to :user
   has_one_attached :image
 
