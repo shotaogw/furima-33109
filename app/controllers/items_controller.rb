117dcd9801360c7b25d7a494ff7e1e01229168b9
shotaogw <shotaogw@gmail.com>
2020-12-30 15:35:56 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:show, :edit, :update]
-  before_action :move_to_index, only: :edit
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.all.order(updated_at: :DESC)
@@ -47,6 +47,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless current_user.id == @item.user_id
+    redirect_to action: :index if current_user.id != @item.user_id
   end
 end
