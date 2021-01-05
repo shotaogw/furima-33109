a0688b83f7344ae2c7c31d16d94e6af217534115
shotaogw <shotaogw@gmail.com>
2021-01-06 00:31:43 +0900

    購入済み商品の処理

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,7 @@
 class PurchasesController < ApplicationController
   before_action :set_item, only: [:index, :create]
   before_action :authenticate_user!
+  before_action :move_to_index, only: [:index, :create]
 
   def index
     @purchase_detail = PurchaseDetail.new
@@ -9,7 +10,7 @@ class PurchasesController < ApplicationController
   def create
     @purchase_detail = PurchaseDetail.new(purchase_params)
     if @purchase_detail.valid?
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp.api_key = ENV['PAYJP_SECRET_KEY']
       Payjp::Charge.create(
         amount: @item.price,
         card: purchase_params[:token],
@@ -32,4 +33,7 @@ class PurchasesController < ApplicationController
     params.require(:purchase_detail).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
+  def move_to_index
+    redirect_to root_path if current_user.id == @item.user_id || @item.purchase.present?
+  end
 end
