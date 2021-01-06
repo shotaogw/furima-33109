837353bb8fe1b22a3ddc7ceccb3d9eb61bfad288
shotaogw <shotaogw@gmail.com>
2021-01-06 20:10:14 +0900

    商品購入機能に関するエラー文の日本語化

--- app/models/purchase_detail.rb
+++ app/models/purchase_detail.rb
@@ -3,11 +3,11 @@ class PurchaseDetail
   attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :postal_code,   format: { with: /\A\d{3}-\d{4}\z/, message: 'input correctly' }
-    validates :prefecture_id, numericality: { other_than: 1, message: 'select' }
+    validates :postal_code,   format: { with: /\A\d{3}-\d{4}\z/, message: 'を正しく入力してください' }
+    validates :prefecture_id, numericality: { other_than: 1, message: 'を選択してください' }
     validates :city
     validates :address
-    validates :phone_number,  numericality: { with: /\d{,11}/, message: 'input only number' }
+    validates :phone_number,  numericality: { with: /\d{,11}/, message: 'を半角数字のみで入力してください' }
     validates :user_id
     validates :item_id
     validates :token
